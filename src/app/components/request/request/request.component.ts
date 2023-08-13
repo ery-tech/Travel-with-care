3afa7e0800809125a09198d94029f67bbc873074
Erika <erikaguardo99@gmail.com>
2023-08-13 20:01:56 +0200

    hosting

--- src/app/components/request/request/request.component.ts
+++ src/app/components/request/request/request.component.ts
@@ -2,27 +2,15 @@ import { Component,  } from '@angular/core';
 import {default as jsonData } from '../../../../json/airports.json';
 import { NgForm } from '@angular/forms';
 import { HttpService } from 'src/app/services/http.service';
-import {
-  trigger, state, style, animate, transition, query, group
- } from '@angular/animations';
-import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-request',
   templateUrl: './request.component.html',
   styleUrls: ['./request.component.css'],
-  animations: [
-    trigger('fade', [
-      state('void', style({ opacity: 0 })),
-      state('*', style({ opacity: 1 })),
 
-      transition(':enter, :leave', [
-        animate(5000)
-      ])
-    ])
-  ]
 })
 export class RequestComponent {
-constructor(private http : HttpService,private router: Router){}
+constructor(private http : HttpService,){}
 
 response:any = ''
 passengers!:number
